e4e6513b8fde2bec2b17ed1c8795683e9f604f68
MJay <mis-ek-j@o2.pl>
2018-03-30 14:09:13 +0200

    Update pomodoro.js

--- pomodoro.js
+++ pomodoro.js
@@ -1,50 +1,61 @@
 var startBool = false;
+var oneNumberMin = true;
 var reset = 25;
 var sec = 59;
 var min = 24;
 var barNumb = 1499;
 var width = 1;
-var seconds=25*60*100;
 var clockInterval;
 var barInterval;
 
+
 window.onload = function() {
-	functionButton(0, "#00cc00", "#009900");
-	functionButton(1, "#ff0000", "#cc0000");
-	functionButton(2, "#bec0b8", "#a9aba0");
+	functionButton("functionButtonStart", 0);
+	functionButton("functionButtonStop",1);
+	functionButton("functionButtonReset", 2);
 	timeButton(25, 1499, 0);
 	timeButton(10, 599, 1);
-	timeButton(5, 300, 2);
+	timeButton(5, 299, 2);
 };
 
 window.onkeydown = function(e){
-	if(e.which)
-	{
+	if(e.which){
 		code=e.which;
-		if(code == 81 && !startBool)
-		{
+		if(code == 81 && !startBool){
+			if(reset <= 10 ){
+				oneNumberMin = false;
+			}
 			startClock();
 		}
-		else if(code == 87 && startBool)
-		{
+		else if(code == 87 && startBool){
 			stopClock();
 		}
 	}
 };
 
-function functionButton(value, color, colorHover){
-	var but = document.getElementsByClassName("functionButton")[value];
-	but.style.background = color;
-	but.onmouseover = function(){
-		but.style.background = colorHover;
-		but.style.transitionDuration - "0.4s";
-	}
-	but.onmouseout = function(){
-		but.style.background = color;
-		but.style.transitionDuration = "0.4s";
-	}
+function functionButton(name, value){
+	var but = document.getElementsByClassName(name)[0];
 	but.onclick = function(){
 		changeTypeOfWork(value);
+	}
+}
+
+function changeTypeOfWork(val){
+	if(val === 0 && startBool == false){
+		if(reset <= 10 ){
+			oneNumberMin = false;
+		}
+		startClock();
+	}
+	else if(val === 1 && startBool == true){
+		stopClock();
+	}
+	else if(val === 2){
+		stopClock();
+		width = 1;
+		min = reset - 1;
+		sec = 59;
+		startClock();
 	}
 }
 		
@@ -56,72 +67,68 @@ function timeButton(time, bar, value){
 	}
 }
 
-function startClock(){
-	clockInterval = setInterval(function(){
-		clock()
-	} , 1000)
-	clockInterval = setInterval(function(){
-		progressBar();
-	} , 1000)
-	startBool = true;
-}
-	
-		
-function stopClock(){
-	clearInterval(clockInterval);
-	clearInterval(clockInterval);
-	startBool = false;
-	document.title = "Pomodoro"; 
-}
-		
 function changeTime(timeGiven){
 	stopClock();
 	clc = document.getElementsByClassName("clock")[0];
 	width = 1;
-	
-	if(timeGiven < 10)
-	{		
-		clc.innerHTML = "0" + timeGiven + " :  00";
-	}
-	else 
-	{
-		clc.innerHTML = timeGiven + " :  00";
-	}
-		
-	reset = timeGiven;
 	sec = 59;
 	min = timeGiven - 1;
+	reset = timeGiven;
+	if(timeGiven <= 10 ){
+		oneNumberMin = false;
+	}
 	startClock();
-}
-		
-function changeTypeOfWork(val){
-	if(val === 0 && startBool == false){
-		startClock();
-	}
-	else if(val === 1 && startBool == true){
-		stopClock();
-	}
-	else if(val === 2){
-		stopClock();
-		clc = document.getElementsByClassName("clock")[0];
-		width = 0;
-				
-		if(reset < 10){ 
-			clc.innerHTML = "0" + reset + " :  00";
-		}
-		else{ 
-			clc.innerHTML = reset + " :  00";
-		}
-			
-		min = reset - 1;
-		sec = 59;
-		startClock();
-	}
-}
+}		
 		
 function clock(){
+	var clc = document.getElementsByClassName("clock")[0];
+	if(oneNumberMin === false){
+		min = "0" + min;
+		oneNumberMin = true;
+	}
+	if(sec < 10 && sec != 0){
+		sec = "0" + sec;
+	}
+	else if(sec === 0 && min != 0){
+		sec = 59;
+		min--;
+		if(min < 10){
+			oneNumberMin = false;
+		}
+	}
+
+    clc.innerHTML = min + " : " + sec;
+	document.title = "(" + min + ":" + sec + ") Pomodoro"; 
+
+    if(sec === 0 && min === "0" + 0){
+		clc.innerHTML = "00 : 00";
+		document.getElementsByClassName("progres")[0].innerHTML = "Done!";
+		document.title = "Buzzzzz!"; 
+        stopClock();
+		var alarm = new Audio("stuff/sound.mp3");
+		alarm.play();
+		min = reset - 1;
+		sec = 59;
+		startBool = false;
+		width = 1;
+	}
+	sec--;
+}
+
+function startClock(){
+	clockInterval = setInterval(function(){
+		clock()
+	} , 1)
+	barInterval = setInterval(function(){
+		progressBar();
+	} , 1)
+	startBool = true;
+}
 	
-	//działanie zegara
+function stopClock(){
+	clearInterval(clockInterval);
+	clearInterval(barInterval);
+	startBool = false;
 }
 		
 function Round(n, k){	
@@ -130,9 +137,9 @@ function Round(n, k){
 }
 		
 function progressBar() {
-	var elem = document.getElementsByClassName("myBar")[0]; 
+	var bar = document.getElementsByClassName("bar")[0]; 
 	var prog = document.getElementsByClassName("progres")[0];
 	width += (100 / barNumb); 
-	elem.style.width = width + '%';
+	bar.style.width = width + '%';
 	prog.innerHTML = Round(width, 0) + "%";	
 }
